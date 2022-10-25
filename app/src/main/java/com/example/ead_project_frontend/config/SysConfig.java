d18daac16325a46fffdcf5ce92aca24d2cf57819
@Ag_ASH <64282593+ahagaash@users.noreply.github.com>
2022-10-26 00:00:37 +0530

    API CHANGES

--- app/src/main/java/com/example/ead_project_frontend/config/SysConfig.java
+++ app/src/main/java/com/example/ead_project_frontend/config/SysConfig.java
@@ -2,5 +2,5 @@ package com.example.ead_project_frontend.config;
 
 public class SysConfig {
 
-    public static String API_URL="http://192.168.1.104:8080/api/";
+    public static String API_URL="http://ahagaash-001-site1.gtempurl.com/api/";
 }
